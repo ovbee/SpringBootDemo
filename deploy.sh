ef25aa1d7cacef0807cb82428d8f3a10cb1809a5
tamjenkins <businessrat@gmail.com>
2019-03-01 21:24:34 +0800

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,8 @@
-app="sbd-1.0"
 echo "Stop and remove container if existing."
-if docker ps | awk -v app="$app" 'NR > 1 && $NF == app{ret=1; exit} END{exit !ret}'; then
-    docker stop "$app" && docker rm -f "$app"
+CONTAINER_NAME="sbd-1.0"
+OLD="$(docker ps --all --quiet --filter=name="$CONTAINER_NAME")"
+if [ -n "$OLD" ]; then
+  docker stop $OLD && docker rm $OLD
 fi
 echo "start container"
-docker run -p 8888:8080 -d --name sbd-1.0 ovbee/spring--boot-demo:1.0
+docker run -p 8888:8080 -d --name $CONTAINER_NAME ovbee/spring--boot-demo:1.0
